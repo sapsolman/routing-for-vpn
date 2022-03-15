d4d424b4bf298dadc96d7d77e6d62be37acf7bfd
sapsolman <35896452+sapsolman@users.noreply.github.com>
2022-03-15 17:11:06 +0300

    Revert "Update modify-vpn-routes.ps1"
    This reverts commit f5bbf0024da3a4c6cc97729ce0f3e000e0857d92.

--- modify-vpn-routes.ps1
+++ modify-vpn-routes.ps1
@@ -1,6 +1,2 @@
-﻿#Allow to execute script
-Set-ExecutionPolicy RemoteSigned -Confirm:$false
-#Fetching routes, saves IF and deletes routes
-Get-NetRoute | Where-Object { $_.NextHop -match "10.*" } | % { $gw=$_.NextHop; $if=$_.InterfaceAlias; $_ | Remove-NetRoute -Confirm:$false }
-#Adding permanent route 10.0.0.0/8 for IF
+﻿Get-NetRoute | Where-Object { $_.NextHop -match "10.*" } | % { $gw=$_.NextHop; $if=$_.InterfaceAlias; $_ | Remove-NetRoute -Confirm:$false }
 New-NetRoute -DestinationPrefix 10.0.0.0/8 -NextHop $gw -InterfaceAlias $if
