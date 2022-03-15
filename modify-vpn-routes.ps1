f5bbf0024da3a4c6cc97729ce0f3e000e0857d92
sapsolman <35896452+sapsolman@users.noreply.github.com>
2022-03-15 16:11:23 +0300

    Update modify-vpn-routes.ps1
    Disabled ExecutionPolicy for RemoteSigned

--- modify-vpn-routes.ps1
+++ modify-vpn-routes.ps1
@@ -1,2 +1,6 @@
-﻿Get-NetRoute | Where-Object { $_.NextHop -match "10.*" } | % { $gw=$_.NextHop; $if=$_.InterfaceAlias; $_ | Remove-NetRoute -Confirm:$false }
+﻿#Allow to execute script
+Set-ExecutionPolicy RemoteSigned -Confirm:$false
+#Fetching routes, saves IF and deletes routes
+Get-NetRoute | Where-Object { $_.NextHop -match "10.*" } | % { $gw=$_.NextHop; $if=$_.InterfaceAlias; $_ | Remove-NetRoute -Confirm:$false }
+#Adding permanent route 10.0.0.0/8 for IF
 New-NetRoute -DestinationPrefix 10.0.0.0/8 -NextHop $gw -InterfaceAlias $if
