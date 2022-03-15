422171630e9c320acdbafb6bfae8a1185a178f85
sapsolman <35896452+sapsolman@users.noreply.github.com>
2022-03-15 11:05:20 +0300

    Create modify-vpn-routes.ps1
    Uploaded first version of PS script.

---
+++ modify-vpn-routes.ps1
@@ -0,0 +1,2 @@
+﻿Get-NetRoute | Where-Object { $_.NextHop -match "10.*" } | % { $gw=$_.NextHop; $if=$_.InterfaceAlias; $_ | Remove-NetRoute -Confirm:$false }
+New-NetRoute -DestinationPrefix 10.0.0.0/8 -NextHop $gw -InterfaceAlias $if
